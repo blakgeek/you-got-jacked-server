5dac00c38c54214aeade650b6d70c62d868076bf
blakgeek <blakgeek@blakgeek.com>
2013-01-13 09:27:24 -0500

    + Added support for rejoining games + Fixed bug where board state was not being tracked which prevented jacking other players.

--- server.js
+++ server.js
@@ -9,23 +9,20 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('find-opponent', function (name) {
 
-        socket.set('name', name);
-
         var game = games.pop();
-        if (game) {
-            console.log("found game: " + game.id);
-            socket.emit('found-game', game.id);
-        } else {
+
+        if (!game) {
             var gameId = uuid();
-            console.log("new game created: " + gameId);
+            console.log("Creating new game: " + gameId);
             game = {
                 id: gameId,
                 player1: name
             };
             createGame(gameId);
             games.push(game);
-            socket.emit('waiting', gameId);
         }
+
+        socket.emit('found-game', game.id);
 
     });
 });
@@ -38,6 +35,7 @@ function createGame(gameId) {
         boardCells = Jax.BOARDS[boardName];
 
     var game = activeGames[gameId] = activeGames[gameId] || {
+        inProgress: false,
         boardName: boardName,
         boardCells: boardCells,
         maxPlayers: 2,
@@ -54,6 +52,48 @@ function createGame(gameId) {
 
         var playerIndex, player;
 
+        socket.on('rejoin', function(index) {
+
+            playerIndex = index;
+            player = game.players[index];
+
+            console.log(player.name + " rejoined");
+
+            socket.emit('joined', boardName, playerIndex);
+            socket.broadcast.emit('opponent-rejoined', player.name, playerIndex);
+
+            if(!game.inProgress) {
+                if (game.playerCnt == 2) {
+
+                    console.log('Starting game: ' + gameId);
+
+                    var p1Cards = game.players[0].cards;
+                    var p2Cards = game.players[1].cards;
+                    for(i = 0; i<7; i++) {
+                        p1Cards.push(game.deck.pop());
+                        p2Cards.push(game.deck.pop());
+                    }
+
+
+                    game.inProgress = true;
+                    socket.broadcast.emit('start-game', boardName, p1Cards);
+                    socket.emit('start-game', boardName, p2Cards);
+                    game.players[0].canPlay = true;
+                    socket.broadcast.emit('take-turn');
+                } else {
+                    socket.emit('waiting-for-players');
+                }
+            } else {
+                socket.emit('game-in-progress', {
+                    boardName: boardName,
+                    cards: player.cards,
+                    cellStates: game.cellStates,
+                    discarded: game.discarded.slice(0,4),
+                    canPlay: player.canPlay
+                })
+            }
+        });
+
         // begin event handler registration
         socket.on('join', function (name) {
             console.log(name + " joined " + gameId);
@@ -67,7 +107,7 @@ function createGame(gameId) {
                 canPlay: false
             }
 
-            socket.broadcast.emit('joined', boardName);
+            socket.emit('joined', boardName, playerIndex);
             socket.broadcast.emit('opponent-joined', name, playerIndex);
 
             game.playerCnt++;
@@ -82,11 +122,13 @@ function createGame(gameId) {
                     p2Cards.push(game.deck.pop());
                 }
 
-
+                game.inProgress = true;
                 socket.broadcast.emit('start-game', boardName, p1Cards);
                 socket.emit('start-game', boardName, p2Cards);
                 game.players[0].canPlay = true;
                 socket.broadcast.emit('take-turn');
+            } else {
+                socket.emit('waiting-for-players');
             }
         });
 
@@ -96,7 +138,7 @@ function createGame(gameId) {
             var drawnCard,
                 card = player.cards[cardIndex];
 
-            if (isValidPlay(cellIndex, cardIndex, player, game)) {
+            if (playCard(cellIndex, cardIndex, player, game)) {
 
                 var drawnCard = game.deck.pop();
                 player.cards.splice(cardIndex, 1, drawnCard);
@@ -110,7 +152,7 @@ function createGame(gameId) {
                 } else {
                     socket.emit('card-drawn', drawnCard, cardIndex);
                     player.canPlay = false;
-                    game.players[(playerIndex + 1) % 2].canPlay = true;
+                    game.players[(+playerIndex + 1) % 2].canPlay = true;
                     socket.broadcast.emit('take-turn');
                 }
             } else {
@@ -126,17 +168,30 @@ function createGame(gameId) {
             socket.broadcast.emit('opponent-discarded-card', discardedCard);
         });
     });
+
+    io.of("/" + gameId).on('disconnect', function() {
+
+        console.log('player disconnected')
+        game.playerCnt--;
+    })
 }
 
-function isValidPlay(cellIndex, playersCardIndex, player, game) {
+function playCard(cellIndex, playersCardIndex, player, game) {
 
     var playersCard = player.cards[playersCardIndex],
         cellCard = game.boardCells[cellIndex],
         cellStates = game.cellStates,
         playersTurn = player.canPlay,
         cardMatchesCell = (Jax.isOpen(cellIndex, cellStates) && (playersCard == cellCard || Jax.isBlackJack(playersCard))),
-        isJackable = (Jax.isRedJack(playersCard) && Jax.isOccupied(cellIndex, cellStates) && !isOccupiedBy(cellIndex, player.flag, game, cellStates));
+        isJackable = (Jax.isRedJack(playersCard) && Jax.isOccupied(cellIndex, cellStates) && !Jax.isOccupiedBy(cellIndex, player.flag, game, cellStates)),
+        // could be optimized to skip all this processing its not the users turn.
+        isValidPlay = (playersTurn && (cardMatchesCell || isJackable));
 
-    // could be optimized to skip all this processing its not the users turn.
-    return (playersTurn && (cardMatchesCell || isJackable));
+    if(isValidPlay && isJackable) {
+        cellStates[cellIndex] = Jax.OPEN_FLAG;
+    } else if(isValidPlay) {
+        cellStates[cellIndex] = player.flag;
+    }
+
+    return isValidPlay;
 }
